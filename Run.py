Have fixed the SFRs so that they are now equivilent on average. Need to figure out peak though. Also cleaned SetupUtil a little.

Run.py
@@ -12,7 +12,6 @@ Outputs: Results.npy - A numpy array of Nxn dimensions (where N = 8 + No. of Tel
 """
 import numpy as np
 import sys
-import matplotlib.pyplot as plt
 
 from Parameters import Parameters
 from SetupUtil import SetupUtil
@@ -165,7 +164,6 @@ class Run:
     self.params.Gas_Mass_Fractions[1] -= M2
     
     self.params.New_Populations_Age[:,:i-1] += self.params.time_in_step
-    self.params.SFH[i-1,:] = [np.sum(self.params.SFR[:self.params.n1]), np.sum(self.params.SFR[self.params.n1:])]
       
   def getFilename(self,i):
 
@@ -196,7 +194,7 @@ def main():
 #def main():
   global directory, directory_results
   global SFR_Algorithm
-  SFR_Algorithm = 1        # Note, a value of 0 corresponds to a delayed tau SF method and 1 is a KS one.
+  SFR_Algorithm = 0        # Note, a value of 0 corresponds to a delayed tau SF method and 1 is a KS one.
   
   directory_results = Imports.Results()
   filter_data = Imports.Filters()
@@ -218,7 +216,7 @@ def main():
       
   #IOUtil.writeParameterFile(params,"tmp.p")
   run.Tracer_Mass, run.Weights = Gas_Dist.Gas_Decision(model,params,run.x0)     # Calculates gas masses and weights and stores them in the self object.
-  run.Population_Mass,run.Initial_Spectral_Density,params.Avg_Population_Mass_1,params.Avg_Population_Mass_2,params.New_Populations_Age,params.SFH = SED.initSED(params.tstart,params.tend,params.h,params.n1,params.n2,run.Weights,params.mass1,
+  run.Population_Mass,run.Initial_Spectral_Density,params.Avg_Population_Mass_1,params.Avg_Population_Mass_2,params.New_Populations_Age = SED.initSED(params.tstart,params.tend,params.h,params.n1,params.n2,run.Weights,params.mass1,
                                                                                                                                                              params.mass2,params.Gas_Fraction_1,params.Gas_Fraction_2,params.Mass_per_Unit,len(Wavelength))
       
   for i in range(1,int(nstep_local+1)):
@@ -236,7 +234,7 @@ def main():
   Population_Flux_Array = Colours.Colour_Calculation(filter_data, Spectral_Density, Wavelength,params.redshift, params.n, params.d_cm)
   
   i = int(nstep_local)
-  
+
   Imports.Export(run.x0,Population_Flux_Array,run.Population_Mass,params.SFR,directory_results,params.Galaxy_Name)
   
 if __name__ == "__main__": main()

SEDs.py
@@ -15,7 +15,6 @@ class SED:
         temp_1 = int(np.ceil(abs((tstart - tend)/time_step) + 2))
         Population_Mass = np.zeros([n, temp_1])  #These lines are required to store the newly created populations initial age.
         Population_Ages = np.zeros([n,temp_1])
-        SFH = np.zeros([temp_1,2])
         
     
         # First, will construct the SED Distribution of the Primary Galaxy for all particles.
@@ -24,7 +23,7 @@ class SED:
         Avg_Population_Mass1 = (1 - Gas_Fraction1)*(Baryonic_Fraction*mass1*MU)*Weights[:n1] 
         Avg_Population_Mass2 = (1 - Gas_Fraction2)*(Baryonic_Fraction*mass2*MU)*Weights[n1:]
         
-        return Population_Mass, Initial_Spectral_Density, Avg_Population_Mass1, Avg_Population_Mass2, Population_Ages, SFH
+        return Population_Mass, Initial_Spectral_Density, Avg_Population_Mass1, Avg_Population_Mass2, Population_Ages
      
         
     def Aging_initSED(n1, n, Age_1, Age_2, Pop1_Mass, Pop2_Mass,Spectral_Density_Array_1,Spectral_Density_Array_2):

SFR_KS.py
@@ -59,7 +59,7 @@ class KS_Model:
             Mass_Used[p] = Total_Mass_Formed
             
             SFR_Array[p] = KS_SFR
-                    
+                                
         M1 = np.sum(Mass_Used[:n1])
         M2 = np.sum(Mass_Used[n1:])
         
@@ -74,15 +74,12 @@ class KS_Model:
       z = Particle_Frame_Coords[:,2]
     
       Radii = np.sqrt(x*x + y*y + z*z)
-
-      criteria_r_index = [x[0] for x in enumerate(Radii) if x[1] <= 2.0]
-      Nearest_Neighbours = Radii[criteria_r_index]
       
-      if np.min(Nearest_Neighbours) == 0 and np.max(Nearest_Neighbours) == 0:
-          Particle_Size = 1
-      else:
-          Particle_Size = np.min(Nearest_Neighbours[np.nonzero(Nearest_Neighbours)])/2
-          
+      Particle_Size = np.min(Radii[np.nonzero(Radii)])/2
+      
+      if Particle_Size > 5:
+          Particle_Size = 5
+      
       Particle_Area = np.pi*(Particle_Size*1e3)**2
-      
+            
       return Particle_Area

SetupUtil.py
@@ -104,7 +104,6 @@ class SetupUtil:
             
       #Need to set the SFR's for each galaxy seperately based on their mass. Low Mass Galaxies have a higher SFR than high mass ones! This will only be temporary
       self.params.New_Pops_Counter = 0
-      self.params.counter = 0
       
 
     self.params.n = self.params.n1 + self.params.n2
@@ -116,18 +115,11 @@ class SetupUtil:
     self.params.Distance_per_Unit = 15  #This is in kpc
     self.params.Distance_per_Unit_m = self.params.Distance_per_Unit*3.086e19
     self.params.Mass_per_Unit = 1e11    #This is in solar masses.
-    self.params.time_in_step = self.params.h*self.params.Time_per_Unit
-
-    
+    self.params.time_in_step = self.params.h*self.params.Time_per_Unit    
     self.params.G = 6.67e-11
-    self.params.Delay_Time_Steps = 1e100
-    self.params.Duration = -1e100
-    self.params.Activate = 0
-    self.params.Deactivate = 1
     
     self.params.Gas_Mass_Fractions = np.zeros(2)
     self.params.Gas_Mass = np.zeros(self.params.n)
-    self.params.Sigma_Gas_Total = np.zeros(self.params.n)
     self.params.SFR = np.zeros(self.params.n)
     self.params.Sigma_Density = np.zeros(self.params.n)
     
@@ -135,9 +127,6 @@ class SetupUtil:
 
     Galaxy_1_Gas_Mass = Baryonic_Fraction*self.params.Gas_Fraction_1*self.params.mass1*self.params.Mass_per_Unit        #This is the Gas Masses in Solar Masses
     Galaxy_2_Gas_Mass = Baryonic_Fraction*self.params.Gas_Fraction_2*self.params.mass2*self.params.Mass_per_Unit
-    
-    self.params.Galaxy_Surface_Area_1_kpc = np.pi*(self.params.rout1*self.params.Distance_per_Unit)**2
-    self.params.Galaxy_Surface_Area_2_kpc = np.pi*(self.params.rout2*self.params.Distance_per_Unit)**2
     
     self.params.Gas_Mass_Fractions[0] = (Galaxy_1_Gas_Mass)           # This is the Gas Surface Area in M_0
     self.params.Gas_Mass_Fractions[1] = (Galaxy_2_Gas_Mass)